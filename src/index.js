0cee71df6805d519f1aff4a21f46e66bd18a1336
Angela <angelalamluu@gmail.com>
2021-08-16 22:57:04 -0700

    added Router component to app

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import App from './components/App'
-import rootReducer from './reducers/rootReducer'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/App';
+import { rootReducer } from './reducers/rootReducer';
 
 //createStore -> 1. reducer 2.dev tools
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root')
 )
