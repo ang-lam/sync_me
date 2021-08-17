7b763499de8ffbd51b73158c3eac657bd22869b2
Angela <angelalamluu@gmail.com>
2021-08-16 22:08:12 -0700

    mount app component in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import App from './components/App'
 
 //createStore -> 1. reducer 2.dev tools
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(applyMiddleware(thunk)))
 
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
