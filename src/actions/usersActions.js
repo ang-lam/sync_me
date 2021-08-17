70c74601d610037c122ea9534647e7602a6ddba6
Angela <angelalamluu@gmail.com>
2021-08-17 14:50:26 -0700

    commented out usersActions

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -3,12 +3,20 @@
 
 //post users (createUser)
 
-export const createUser = user => {
-    fetch('http://127.0.0.1:3000/users', {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: { 'Content-Type': 'application/json'}
-    })
-        .then (resp => resp.json())
-        .then (user => dispatch({ type: 'CREATE_USER', payload: user}))
-}
+// export const userActions = {
+//     login,
+//     logout,
+//     register,
+//     getAll,
+//     delete: _delete
+// }
+
+// const createUser = user => {
+//     fetch('http://127.0.0.1:3000/users', {
+//         method: 'POST',
+//         body: JSON.stringify(user),
+//         headers: { 'Content-Type': 'application/json'}
+//     })
+//         .then (resp => resp.json())
+//         .then (user => dispatch({ type: 'CREATE_USER', payload: user}))
+// }
