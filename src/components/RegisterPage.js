48965c502e501a3c3c4383432fba63643c88487b
Angela <angelalamluu@gmail.com>
2021-08-17 14:33:23 -0700

    created state in RegisterPage component

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class RegisterPage extends Component {
+
+    state = {
+        user: {
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: ''
+        },
+        submitted: false
+    };
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default RegisterPage
