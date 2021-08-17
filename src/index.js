0c748f45e30b6412754b1aba612e9c7de33f8204
Angela <angelalamluu@gmail.com>
2021-08-16 22:06:32 -0700

    declared store variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 //react-redux, redux, thunk
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+//createStore -> 1. reducer 2.dev tools
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(applyMiddleware(thunk)))
+
