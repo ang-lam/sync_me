501299e5139e09d93c1f471bbdf3f98f70b8d8f7
Angela <angelalamluu@gmail.com>
2021-08-16 23:10:26 -0700

    created router.js

---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from '../components/Home'
+//import the containers for the 
+
+const Router = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Router
+
