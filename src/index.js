cf450e110210faeaf503e957fa47f4e2c66eb651
Angela <angelalamluu@gmail.com>
2021-08-16 22:45:44 -0700

    imported rootReducer to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './components/App'
+import rootReducer from './reducers/rootReducer'
 
 //createStore -> 1. reducer 2.dev tools
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(applyMiddleware(thunk)))
