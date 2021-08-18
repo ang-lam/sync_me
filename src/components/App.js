15b345bef962a475b6702577f418fd0862d0110f
Angela <angelalamluu@gmail.com>
2021-08-17 21:48:33 -0700

    moved registerpage component to router.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Router from './Router'
 import NavBar from './NavBar'
-import RegisterPage from './RegisterPage'
 
 const App = () => {
     return (
         <div className="ui container">
             <NavBar />
-            <RegisterPage />
             <Router />
         </div>
     )
