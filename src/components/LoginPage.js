08f35305b435b84ba581662356019c1ca83d1840
Angela <angelalamluu@gmail.com>
2021-08-17 21:45:00 -0700

    created login page component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { login } from '../actions/usersActions'
+
+class LoginPage extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleFormChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.login(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <label>Email:</label>
+                    <input
+                        onChange={this.handleFormChange}
+                        type="text"
+                        name="email"
+                        value={this.state.email}
+                    /> <br />
+                    <label>Password:</label>
+                    <input
+                        onChange={this.handleFormChange}
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                    /> <br />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: userInfo => dispatch(login(userInfo))
+    }
+}
+export default connect(null, mapDispatchToProps)(LoginPage)
