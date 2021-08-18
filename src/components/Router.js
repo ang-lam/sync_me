3eb73f7c10ddd41276c3d5938b545850ff3cbc64
Angela <angelalamluu@gmail.com>
2021-08-17 21:44:37 -0700

    added routes for signup and home

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { login } from '../actions/usersActions'
 import Home from '../components/Home'
+import RegisterPage from './RegisterPage'
+import LoginPage from './LoginPage'
+
 //import the containers for the 
 
 const Router = () => {
     return (
         <div>
             <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/test' />
+                <Route exact path='/' component={LoginPage} />
+                <Route exact path='/signup' component={RegisterPage}/>
+                <Route exact path='/home' component={Home} />
             </Switch>
             
         </div>
