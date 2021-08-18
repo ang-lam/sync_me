f84e968bb30502eeb453081ff1943e4d1cf0fa12
Angela <angelalamluu@gmail.com>
2021-08-17 22:37:37 -0700

    added fetch_user type to userReducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -5,12 +5,13 @@ const initialState = {
 
 export const usersReducer = (state = initialState, action) => {
     switch(action.type){
-        // case 'FETCH_USERS':
-        //     return action.payload
+        case 'FETCH_USERS':
+            return {...state, allUsers: [...action.payload]}
         // case 'CREATE_USER':
         //     return [...state, action.payload]
         case 'LOGIN_USER':
             const newUser = state.allUsers.find(user => user.id === action.payload.id)
+            debugger
                 if (newUser) {
                     return {...state, currentUser: action.payload }
                 } else {
