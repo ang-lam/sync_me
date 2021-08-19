dbf5fa74e783c6592c53d6ee2c2c4f6704ad2739
Angela <angelalamluu@gmail.com>
2021-08-18 22:46:54 -0700

    created userCard with props

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,14 +3,31 @@ import { connect } from 'react-redux'
 
 const UserCard = props => {
     return (
-        <div>
-            
+        //className='ui link cards' for wrapping component
+        <div class="card">
+            <div className="image">
+                <img src="https://i.pinimg.com/originals/28/d4/62/28d462f90eca234062f8a2ebba8c8fe1.jpg" alt="avatar"/>
+            </div>
+            <div className="content">
+                <div className="header">{props.firstName} {props.lastName}</div>
+                <div className="meta">
+                    {props.company}
+                </div>
+                <div className="description">
+                    {props.bio}
+                </div>
+            </div>
+            <div className="extra content">
+                <span className="right floated">
+                    <button>SYNC</button>
+                </span>
+            </div>
         </div>
     )
 }
 
 const mapStateToProps = state => {
-    
+
 }
 
 export default connect(mapStateToProps)(UserCard)
