d62f3af660c22e77071d9ed9d7766d73a21cf3a1
Angela <angelalamluu@gmail.com>
2021-08-18 22:47:17 -0700

    mount userCard component in home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import UserCard from './UserCard';
 
 const Home = (props) => {
     console.log('home:', props)
     const {firstName, lastName, company, bio} = props.user
     return (
-        <div>
-            Home <br />
+        <div className="ui link cards">
+            {/* Home <br />
             Name: {firstName} {lastName} <br />
             Comapny: {company} <br />
-            Bio: {bio}
+            Bio: {bio} */}
+            < UserCard firstName={firstName} lastName={lastName} company={company} bio={bio}/>
         </div>
     )
 }
