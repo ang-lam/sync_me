5ba27b6305b6fb65a3ce6a9c46cdb237b22c77f8
Angela <angelalamluu@gmail.com>
2021-08-21 01:10:47 -0700

    created route and component for syncs

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,6 +5,7 @@ import RegisterPage from './RegisterPage'
 import LoginPage from './LoginPage'
 import AuthRoute from './AuthRoute'
 import AllSyncs from '../containers/AllSyncs'
+import Syncs from '../containers/Syncs'
 
 //import the containers for the 
 
@@ -17,6 +18,7 @@ const Router = () => {
                 <Route exact path='/home' component={Home} />
                 <Route exact path='/connect' component={AllSyncs} />
                 <Route exact path='/signup' component={ (routeInfo) => <RegisterPage routeInfo={routeInfo} />} />
+                <Route exact path='/syncs' component={Syncs} />
                 {/* <AuthRoute exact path='/'>
                     <LoginPage />
                 </AuthRoute> */}
---
+++ src/containers/Syncs.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import UserCard from '../components/UserCard'
+
+const AllSyncs = props => {
+    //add conditional based off mentee or mentor
+    debugger
+    const allCardsJSX = props.user.mentors.map( user => {
+        return (
+            <UserCard key={user.id}firstName={user.firstName} lastName={user.lastName} company={user.company} bio={user.bio} />
+        )
+    })
+    return (
+        <div className="ui link cards">
+            {allCardsJSX}
+        </div>
+    )
+}
+
+const mapDispatchToProps = state => {
+    // return ({users: state.users.allUsers})
+    return ({user: state.users.currentUser})
+}
+
+export default connect(mapDispatchToProps)(AllSyncs)
