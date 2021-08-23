891829980d22a42a5fd6c6a4edf254ea011e09a9
Angela <angelalamluu@gmail.com>
2021-08-23 00:39:36 -0700

    removed redirect import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Redirect } from "react-router-dom"
 import { logoutUser } from '../actions/usersActions'
 
 class NavBar extends Component {
