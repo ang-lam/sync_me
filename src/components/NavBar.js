86ec9a46877093cc50ab8a443db3c626e2bde333
Angela <angelalamluu@gmail.com>
2021-08-22 23:40:52 -0700

    changed navbar to class comp and imported action

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logoutUser } from '../actions/usersActions'
 
-const NavBar = () => {
-    return (
-        <div className="ui segment">
-            <Link to='/home'>Home</Link> |
-            <Link to='/syncs'>Users Syncs</Link> |
-            <Link to='/connect'>Sync</Link> |
-            <Link to='/'>Login for test</Link> |
-            <Link to='/signup'>Signup</Link>
-        </div>
-    )
+class NavBar extends Component {
+    handleClick = e => {
+        e.preventDefault()
+        //remove token from localStorage
+        localStorage.removeItem('token')
+        //remove user object from redux store
+        this.props.logoutUser()
+    }
+
+    render(){
+        return (
+            <div className="ui segment">
+                <Link to='/home'>Home</Link> |
+                <Link to='/syncs'>Users Syncs</Link> |
+                <Link to='/connect'>Sync</Link> |
+                <Link to='/'>Login for test</Link> |
+                <Link to='/signup'>Signup</Link> |
+                <button onClick={this.handleClick}>Logout</button>
+            </div>
+        )
+    }
+    
 }
 
-export default NavBar
+export default connect(null, { logoutUser })(NavBar)
