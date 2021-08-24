49c667ed1a0025bff8e48cc820997852dea2521b
Angela <angelalamluu@gmail.com>
2021-08-23 18:00:29 -0700

    reverted router component back to functional

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import Home from '../components/Home'
 import RegisterPage from './RegisterPage'
@@ -10,35 +10,31 @@ import Logout from './Logout';
 
 //import the containers for the 
 
-class Router extends Component {
-
-    render() {
-        return (
-            <div>
-                <Switch>
-                    <Route exact path='/' component={ (routeInfo) => <LoginPage routeInfo={routeInfo} />} />
-                    {/* <Route exact path='/signup' component={RegisterPage}/> */}
-                    <Route exact path='/home' component={Home} />
-                    <Route exact path='/connect' component={AllSyncs} />
-                    <Route exact path='/signup' component={ (routeInfo) => <RegisterPage routeInfo={routeInfo} />} />
-                    <Route exact path='/syncs' component={Syncs} />
-                    <Route exact path='/logout' component={Logout} />
-                    {/* <AuthRoute exact path='/'>
-                        <LoginPage />
-                    </AuthRoute> */}
-                    {/* <AuthRoute exact path='/signup'>
-                        <RegisterPage />
-                    </AuthRoute> */}
-                    {/* <AuthRoute exact path='/home' type='private'>
-                        <Home />
-                    </AuthRoute> */}
-                </Switch>
-                
-            </div>
-        )
-    }
-    
+const Router = () => {
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={ (routeInfo) => <LoginPage routeInfo={routeInfo} />} />
+                {/* <Route exact path='/signup' component={RegisterPage}/> */}
+                <Route exact path='/home' component={Home} />
+                <Route exact path='/connect' component={AllSyncs} />
+                <Route exact path='/signup' component={ (routeInfo) => <RegisterPage routeInfo={routeInfo} />} />
+                <Route exact path='/syncs' component={Syncs} />
+                <Route exact path='/logout' component={Logout} />
+                {/* <AuthRoute exact path='/'>
+                    <LoginPage />
+                </AuthRoute> */}
+                {/* <AuthRoute exact path='/signup'>
+                    <RegisterPage />
+                </AuthRoute> */}
+                {/* <AuthRoute exact path='/home' type='private'>
+                    <Home />
+                </AuthRoute> */}
+            </Switch>
+            
+        </div>
+    )
 }
-
+    
 export default Router
 
