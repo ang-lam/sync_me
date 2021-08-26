d54f6f0a538882f9af37c30fc802423620dca97e
Angela <angelalamluu@gmail.com>
2021-08-26 01:15:50 -0700

    created input field for new posts

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,11 +5,31 @@ class InputField extends Component {
     state = {
         input: ''
     }
-    
+
+    handleInputChange = e => {
+        this.setState({
+            input: e.target.value
+        })
+    }
+
+    handleFormSubmit = e => {
+        e.preventDefault()
+        this.props.onSearchSubmit(this.state.input)   
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="ui segment">
+                <form onSubmit={this.handleFormSubmit} className="ui form">
+                    <div className="field">
+                        {/* this should be a prop */}
+                        <label>What's on your mind?</label>
+                        <input type='text' 
+                        value={this.state.input}
+                        onChange={this.handleInputChange}/>
+                    </div>
+                    <button className="ui button" type="submit">Post</button>
+                </form> 
             </div>
         )
     }
