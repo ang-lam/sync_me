83699022807ec7158160229d5845c64993c461c8
Angela <angelalamluu@gmail.com>
2021-08-25 17:59:12 -0700

    imported postsReducer into rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { postsReducer } from './postsReducer';
 import { usersReducer } from './usersReducer';
 //import all reducers here
 
 export const rootReducer = combineReducers({
     //keys for the state -> users: usersReducer
-    users: usersReducer
+    users: usersReducer,
+    posts: postsReducer
 })
