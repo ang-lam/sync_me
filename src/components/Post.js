4207d2000a50458530ee1e616ff648b41fbce60d
Angela <angelalamluu@gmail.com>
2021-08-26 01:01:57 -0700

    styled post feed

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,17 +4,14 @@ const Post = props => {
     return (
         <div className="ui segment">
             <div className="content">
-                <div className="summary">
+                <div className="author">
                      {props.user.firstName} {props.user.lastName}
-                    <div className="date">
+                </div>
+                    <span className="date">
                         3 days ago
-                    </div>
-                </div>
-                <div className="extra text">
-                    <h3>{props.content}</h3>
-                </div>
-                <div className="meta">
-                    <i className="like icon"></i> 5 Likes
+                    </span>
+                <div className="text">
+                    <h4>{props.content}</h4>
                 </div>
             </div>
         </div>
