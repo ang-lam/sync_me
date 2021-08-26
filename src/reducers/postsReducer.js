1316f0614ea347d3bce38e511adf473ef1bbeb9b
Angela <angelalamluu@gmail.com>
2021-08-25 17:58:34 -0700

    created postsReducer with fetch post action

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    posts: []
+}
+
+export const postsReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'FETCH_POSTS':
+            return {...state, posts: action.payload}
+        default:
+            return state
+    }
+
+}
