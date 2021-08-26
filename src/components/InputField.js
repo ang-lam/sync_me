8a873ff916b3540fd8f9ad122a779af6af509e3f
Angela <angelalamluu@gmail.com>
2021-08-26 01:08:41 -0700

    created inputfield component

---
+++ src/components/InputField.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class InputField extends Component {
+
+    state = {
+        input: ''
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default InputField
