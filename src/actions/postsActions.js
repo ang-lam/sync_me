be80986739e6f809746a50f6aaaf7965d4cbcf44
Angela <angelalamluu@gmail.com>
2021-08-26 22:08:27 -0700

    created createPost action

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -5,3 +5,17 @@ export const fetchPosts = () => {
             .then(data => dispatch({ type: 'FETCH_POSTS', payload: data}))
     }
 }
+
+export const createPost = post => {
+    return dispatch => {
+        fetch('http://127.0.0.1:3000/posts', {
+            method: 'POST',
+            body: JSON.stringify({post}),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            dispatch({ type: 'CREATE_POST', payload: data })
+        })
+    }
+}
