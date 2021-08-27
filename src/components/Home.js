df036ff65e7c5c56b273e59abf822a0e889abf70
Angela <angelalamluu@gmail.com>
2021-08-26 23:12:59 -0700

    removed unnecessary code from Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import InputField from './InputField';
 import Login from './Login';
-import Post from './Post';
 
 const Home = (props) => {
-    const followedIds = props.followed.map ( user => user.id )
-    const followedPosts = props.posts.filter( post => {
-        return followedIds.includes(post.user_id) || post.user_id === props.user.id
-    })
-    const chronologicalPosts = followedPosts.reverse()
-    const postsJSX = chronologicalPosts.map(post => {
-        return <Post key={post.id} userId={post.user_id} content={post.content} user={post.user}/>
-    })
-
     return (
         <div className="ui large feed">
             
@@ -26,11 +15,7 @@ const Home = (props) => {
 }
 
 const mapStateToProps = state => {
-    return ({user: state.users.currentUser,
-            followed: state.users.followed, 
-            loggedIn: state.users.loggedIn,
-            posts: state.posts.posts
-    })
+    return ({loggedIn: state.users.loggedIn})
 }
 
 export default connect(mapStateToProps)(Home)
