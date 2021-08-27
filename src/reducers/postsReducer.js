824dda1215d3564231bc607d223c210cc1eb65c6
Angela <angelalamluu@gmail.com>
2021-08-26 22:09:59 -0700

    added create_post case to postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -6,6 +6,8 @@ export const postsReducer = (state = initialState, action) => {
     switch(action.type){
         case 'FETCH_POSTS':
             return {...state, posts: action.payload}
+        case 'CREATE_POST':
+            return {...state, posts: [...state.posts, action.payload]}
         default:
             return state
     }
