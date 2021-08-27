59f093e7f58b0bf86243ccead4c217a0f254dcd5
Angela <angelalamluu@gmail.com>
2021-08-27 01:03:38 -0700

    added linkedin and github icons that link externally to my profiles

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -6,6 +6,12 @@ const AppInfo = () => {
         <div className="ui segment">
             <p>Sync, short for synchronize, means to happen at the same time.</p>
             <p>Sync Me is social media for all techies. Connect with your fellow techies in the world and follow what they have to say.</p>
+            <a href="https://www.github.com/ang-lam" target="_blank" rel="noreferrer">
+                <i className="github large icon"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/anglam" target="_blank" rel="noreferrer">
+                <i className="linkedin large icon"></i>
+            </a>
         </div>
     )
 }
