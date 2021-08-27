5ed7cbc9b619a3274c37ded91aaae589f9de90ba
Angela <angelalamluu@gmail.com>
2021-08-27 13:31:02 -0700

    removed removeUser logic from login reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -14,21 +14,25 @@ export const usersReducer = (state = initialState, action) => {
         case 'LOGIN_USER':
             const existingUser = state.allUsers.find(user => user.id === action.payload.id)
                 if (!!existingUser) {
-                    console.log('existing', existingUser)
-                    const removedUser = state.allUsers.filter( user => {
-                        const userIds = existingUser.followed.map ( user => user.id)
-                        return !userIds.includes(user.id) && user !== existingUser})
-                    return {...state, 
-                            allUsers: removedUser,
+                    return {...state,
                             currentUser: existingUser,
                             loggedIn: true,
                             followed: existingUser.followed
                         }
+                    // const removedUser = state.allUsers.filter( user => {
+                    //     const userIds = existingUser.followed.map ( user => user.id)
+                    //     return !userIds.includes(user.id) && user !== existingUser})
+                    // return {...state, 
+                    //         allUsers: removedUser,
+                    //         currentUser: existingUser,
+                    //         loggedIn: true,
+                    //         followed: existingUser.followed
+                    //     }
                 } else {
                     return {...state, currentUser: action.payload, loggedIn: true}
                 }; 
-        case 'LOG_USER': //remove this?
-            return {...state, loggedIn: !state.loggedIn, followed: []}
+        // case 'LOG_USER': //remove this?
+        //     return {...state, loggedIn: !state.loggedIn, followed: []}
         case 'LOGOUT_USER':
             return {...state, currentUser: {}, loggedIn: false, followed: []}
         case 'FOLLOW_USER':
