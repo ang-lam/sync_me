21e4d5b71b102f64f6a68f0c3402142280317164
Angela <angelalamluu@gmail.com>
2021-08-27 12:11:55 -0700

    applied css to home page

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -4,14 +4,14 @@ import '../stylesheets/AppInfo.css'
 const AppInfo = () => {
     return (
         <div className="ui segment">
-            <p>Sync, short for synchronize, means to happen at the same time.</p>
-            <p>Sync Me is social media for all techies. Connect with your fellow techies in the world and follow what they have to say.</p>
-            <a href="https://www.github.com/ang-lam" target="_blank" rel="noreferrer">
+            <p>Sync, short for synchronize, means to happen at the same time. There are thousands of other techies like you waiting to connect.</p>
+            <p>Sync Me is social media for all techies to sync with each other and share their knowledge and thoughts. Connect with your fellow techies in the world and follow what they have to say.</p>
+            <p><a href="https://www.github.com/ang-lam" target="_blank" rel="noreferrer">
                 <i className="github large icon"></i>
             </a>
             <a href="https://www.linkedin.com/in/anglam" target="_blank" rel="noreferrer">
                 <i className="linkedin large icon"></i>
-            </a>
+            </a></p>
         </div>
     )
 }
