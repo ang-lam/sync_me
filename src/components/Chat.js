bf3ca93e5a9ec03c2435b1ab9d884bda9c3a96ac
Angela <angelalamluu@gmail.com>
2021-08-31 00:34:24 -0700

    created chat component and comments for implementing chat

---
+++ src/components/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Chat = () => {
+    return (
+        //get all messages from backend and store in store 
+        //iterate through all messages to match the two users through sender_id and user_id
+        //make a message component that renders each message on the chat page
+        //have input at bottom of chat page with send button 
+        //send button should make a post request to backend to create new message with sender_id === currentUser.id and recipient_id === followed_id
+        //chat should be class component with state? need to send following user ID as prop? to chat component 
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chat
