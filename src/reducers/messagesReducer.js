ade03f12d4f54cf9ef16e49ca8ab3352efc7dedb
Angela <angelalamluu@gmail.com>
2021-09-01 13:35:13 -0700

    case for create_message adds payload to messages state

--- src/reducers/messagesReducer.js
+++ src/reducers/messagesReducer.js
@@ -6,6 +6,8 @@ export const messagesReducer = (state = initialState, action) => {
     switch(action.type){
         case 'FETCH_MESSAGES':
             return {...state, messages: action.payload}
+        case 'CREATE_MESSAGE':
+            return {...state, messages: [...state.messages, action.payload]}
         default:
             return state
     }
