029a78d600f2cb11e9f335730d7b3accc9ace37c
Angela <angelalamluu@gmail.com>
2021-09-02 00:20:44 -0700

    removed commented out old code from usersReducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -19,27 +19,15 @@ export const usersReducer = (state = initialState, action) => {
                             loggedIn: true,
                             followed: existingUser.followed
                         }
-                    // const removedUser = state.allUsers.filter( user => {
-                    //     const userIds = existingUser.followed.map ( user => user.id)
-                    //     return !userIds.includes(user.id) && user !== existingUser})
-                    // return {...state, 
-                    //         allUsers: removedUser,
-                    //         currentUser: existingUser,
-                    //         loggedIn: true,
-                    //         followed: existingUser.followed
-                    //     }
                 } else {
                     return {...state, currentUser: action.payload, loggedIn: true}
                 }; 
-        // case 'LOG_USER': //remove this?
-        //     return {...state, loggedIn: !state.loggedIn, followed: []}
         case 'LOGOUT_USER':
             return {...state, currentUser: {}, loggedIn: false, followed: []}
         case 'FOLLOW_USER':
             const removeUser = state.allUsers.filter( user => user.id !== action.payload.connection.followed_id)
             return {...state, allUsers: removeUser, followed: [...state.followed, action.payload.user]}
         case 'UNFOLLOW_USER':
-            //add removed user back to allUsers and remove from followed
             const unfollowedUser = state.followed.find ( user => user.id === action.payload )
             const updatedFollowed = state.followed.filter( user => user.id !== action.payload)
             return {...state, allUsers: [...state.allUsers, unfollowedUser], followed: updatedFollowed }
