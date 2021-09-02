c0f7855024d91a771143c83d42d695bec7af9be8
Angela <angelalamluu@gmail.com>
2021-09-02 00:17:08 -0700

    server url into a constant and refactored fetch requests

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,6 +1,8 @@
+const apiUrl = 'http://127.0.0.1:3000'
+
 export const fetchPosts = () => {
     return dispatch => {
-        fetch('http://127.0.0.1:3000/posts')
+        fetch(`${apiUrl}/posts`)
             .then(resp => resp.json())
             .then(data => dispatch({ type: 'FETCH_POSTS', payload: data}))
     }
@@ -12,7 +14,7 @@ export const createPost = (content, user) => {
         content: content
     }
     return dispatch => {
-        fetch('http://127.0.0.1:3000/posts', {
+        fetch(`${apiUrl}/posts`, {
             method: 'POST',
             body: JSON.stringify({post}),
             headers: { 'Content-Type': 'application/json'}
