cf533d7fa66735cf0eeda4e7c14ac1571f77f656
Angela <angelalamluu@gmail.com>
2021-09-02 17:23:57 -0700

    display last message under users name

--- src/components/Messages/Chatroom.js
+++ src/components/Messages/Chatroom.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Chatroom = () => {
+const Chatroom = props => {
+    const sortByRecent = props.user.messages.reverse()
+    const lastMessage = sortByRecent.find( message => message.sender_id === props.user.id || message.recipient_id === props.user.id)
+    
+
     return (
-        <div>
-            
+        <div className="ui segment">
+            <h3>{props.user.firstName} {props.user.lastName}</h3>
+            <div></div>
+            {lastMessage.content}
         </div>
     )
 }
